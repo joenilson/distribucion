687dd539a3377e4e20ebc65206bdf8606c307e54
Joe Nilson <joenilson@gmail.com>
2015-10-14 19:05:45 -0400

    first Commit

---
+++ controller/distribucion_conductores.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * Copyright (C) 2015 Joe Nilson <joenilson@gmail.com>
+ *
+ *  * This program is free software: you can redistribute it and/or modify
+ *  * it under the terms of the GNU Affero General Public License as
+ *  * published by the Free Software Foundation, either version 3 of the
+ *  * License, or (at your option) any later version.
+ *  *
+ *  * This program is distributed in the hope that it will be useful,
+ *  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See th * e
+ *  * GNU Affero General Public License for more details.
+ *  * 
+ *  * You should have received a copy of the GNU Affero General Public License
+ *  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+ */
+
+/**
+ * Description of distribucion_conductores
+ *
+ * @author Joe Nilson <joenilson@gmail.com>
+ */
+class distribucion_conductores extends fs_controller {
+    public function __construct() {
+        parent::__construct(__CLASS__, '3 - Conductores', 'distribucion');
+    }
+    
+    public function private_core(){
+        
+    }
+}
