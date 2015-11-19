3414a386b671e944aacce45809393c03654e72e0
Joe Nilson <joenilson@gmail.com>
2015-11-19 18:06:52 -0400

    Agregada estructura para rutas, completando el guardado de la información de frecuencia de visita

--- controller/distrib_clientes.php
+++ controller/distrib_clientes.php
@@ -21,6 +21,7 @@ require_model('cliente.php');
 require_model('almacen.php');
 require_model('distribucion_agente.php');
 require_model('distribucion_organizacion.php');
+require_model('distribucion_rutas.php');
 /**
  * Description of distribucion_creacion
  *
@@ -33,9 +34,12 @@ class distrib_clientes extends fs_controller {
     public $supervisor;
     public $vendedor;
     public $agente;
+    public $rutas;
+    public $type;
     public $cliente_datos;
     public $distribucion_agente;
     public $distribucion_organizacion;
+    public $distribucion_rutas;
     public $supervisores_asignados;
     public $supervisores_libres;
     public $vendedores_asignados;
@@ -52,6 +56,7 @@ class distrib_clientes extends fs_controller {
         $this->almacen = new almacen();
         $this->agente = new distribucion_agente();
         $this->distribucion_organizacion = new distribucion_organizacion();
+        $this->distribucion_rutas = new distribucion_rutas();
 
         $type = \filter_input(INPUT_POST, 'type');
         $codcliente = \filter_input(INPUT_GET, 'codcliente');
@@ -101,12 +106,59 @@ class distrib_clientes extends fs_controller {
             } else {
                 $this->new_error_msg("¡Imposible tratar los datos del ".$agente0->tipoagente."!");
             }
+        }elseif($type=='ruta'){
+            $codagente = \filter_input(INPUT_POST, 'codagente');
+            $ruta = \filter_input(INPUT_POST, 'ruta');
+            $descripcion = \filter_input(INPUT_POST, 'descripcion');
+            $data_agente = $this->distribucion_organizacion->get($this->empresa->id, $codagente);
+            //echo "Aqui: ".$data_agente;
+            $codalmacen = $data_agente[0]->codalmacen;
+            $lunes_val = \filter_input(INPUT_POST, 'lunes');
+            $martes_val = \filter_input(INPUT_POST, 'martes');
+            $miercoles_val = \filter_input(INPUT_POST, 'miercoles');
+            $jueves_val = \filter_input(INPUT_POST, 'jueves');
+            $viernes_val = \filter_input(INPUT_POST, 'viernes');
+            $sabado_val = \filter_input(INPUT_POST, 'sabado');
+            $domingo_val = \filter_input(INPUT_POST, 'domingo');
+            $estado_val = \filter_input(INPUT_POST, 'estado');
+            $lunes = (isset($lunes_val))?"true":"false";
+            $martes = (isset($martes_val))?"true":"false";
+            $miercoles = (isset($miercoles_val))?true:false;
+            $jueves = (isset($jueves_val))?true:false;
+            $viernes = (isset($viernes_val))?true:false;
+            $sabado = (isset($sabado_val))?true:false;
+            $domingo = (isset($domingo_val))?true:false;
+            $estado = (isset($estado_val))?true:false;
+            $ruta0 = new distribucion_rutas();
+            $ruta0->idempresa = $this->empresa->id;
+            $ruta0->codalmacen = $codalmacen;
+            $ruta0->codagente = $codagente;
+            $ruta0->ruta = $ruta;
+            $ruta0->descripcion = trim($descripcion);
+            $ruta0->lunes = trim($lunes);
+            $ruta0->martes = trim($martes);
+            $ruta0->miercoles = trim($miercoles);
+            $ruta0->jueves = trim($jueves);
+            $ruta0->viernes = trim($viernes);
+            $ruta0->sabado = trim($sabado);
+            $ruta0->domingo = trim($domingo);
+            $ruta0->estado = trim($estado);
+            $ruta0->usuario_creacion = $this->user->nick;
+            $ruta0->fecha_creacion = \Date('d-m-Y H:i:s');
+            if($ruta0->save()){
+                $this->new_message("$ruta0->ruta creada correctamente.");
+            }else{
+                $this->new_error_msg("¡Imposible crear la ruta con los datos seleccionados!");
+            }
         }
+        $this->type = $type;
         $this->supervisores_asignados = $this->distribucion_organizacion->all_tipoagente($this->empresa->id, 'SUPERVISOR');
         $this->supervisores_libres = $this->agente->get_activos_por('cargo','SUPERVISOR');
         
         $this->vendedores_asignados = $this->distribucion_organizacion->all_tipoagente($this->empresa->id, 'VENDEDOR');
         $this->vendedores_libres = $this->agente->get_activos_por('cargo','VENDEDOR');
+        
+        $this->rutas = $this->distribucion_rutas->all($this->empresa->id);
 
     }
     
