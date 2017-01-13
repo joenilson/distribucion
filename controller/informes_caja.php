d4219e238869955fa0b9db313275ea31276ecb5d
Joe Nilson <joenilson@gmail.com>
2017-01-13 17:48:30 -0400

    distribucion: informes_caja reformulación de la data, se va generar una estructura mas limpia de los querys

--- controller/informes_caja.php
+++ controller/informes_caja.php
@@ -19,7 +19,7 @@
 require_model('almacenes.php');
 require_model('distribucion_faltantes.php');
 require_model('facturas_cliente.php');
-require_model('facturas_cliente.php');
+require_model('facturas_proveedor.php');
 require_model('forma_pago.php');
 /**
  * Description of informes_caja
@@ -39,6 +39,7 @@ class informes_caja extends fs_controller {
     public $egresos;
     public $cobros;
     public $resultados_egresos;
+    public $resultados_egresos_formas_pago;
     public $resultados_formas_pago;
     public $resultados_faltantes_cobrados;
     public $resultados_faltantes_pendientes;
@@ -75,15 +76,93 @@ class informes_caja extends fs_controller {
                     $this->resultados_formas_pago = $resultados['formas_pago'];
                     $this->resultados_faltantes_cobrados = $resultados['faltantes_cobrados'];
                     $this->resultados_faltantes_pendientes = $resultados['faltantes_pendientes'];
+                    $this->resultados_egresos_formas_pago = $resultados['egresos_formas_pago'];
                     $this->resultados_egresos = $resultados['egresos'];
                     break;
             }
         }
     }
+    
+    private function movimientos(){
+        
+    }
+    
+    private function ingresos(){
+        
+    }
+    
+    private function egresos(){
+        
+    }
+    
+    private function cobros(){
+        
+    }
+    
+    private function pendientes_cobro(){
+        
+    }
    
     private function resumen_movimientos(){
-        //Obtenemos las ventas que no estén anuladas y sacamos las que estén o no pagadas
         $fp = new FacturaScripts\model\forma_pago;
+        //Obtenemos las compras que no estén anuladas y sacamos las que estén o no pagadas
+        $query_compras = "fecha >= ".$this->facturascli->var2str(\date('Y-m-d',strtotime($this->f_desde)))
+                ." AND fecha <= ".$this->facturascli->var2str(\date('Y-m-d',strtotime($this->f_hasta)))
+                ." AND codalmacen = ".$this->facturascli->var2str($this->codalmacen)
+                ." AND anulada = FALSE ORDER BY fecha";
+        $sql_compras = "SELECT * FROM facturasprov WHERE $query_compras";
+        $this->new_advice($sql_compras);
+        $lista_egresos = $this->db->select($sql_compras);
+        $resultados_egresos_formas_pago = array();
+        //Obtenemos las facturas de compra por pagar
+        if($lista_egresos){
+            $formas_pago = array();
+            $facturasprov_formas_pago = array();
+            $facturasprov_pagadas = array();
+            $facturasprov_porpagar = array();
+            foreach($lista_egresos as $f){
+                $fac = new factura_proveedor($f);
+                $pago_egreso = $fac->get_asiento_pago();
+                if(\date('Y-m-d',strtotime($pago_egreso->fecha))>=\date('Y-m-d',strtotime($this->f_desde)) AND \date('Y-m-d',strtotime($pago_egreso->fecha))<=\date('Y-m-d',strtotime($this->f_hasta))){
+                    //Esta pagada
+                }else{
+                    //Esta pendiente
+                }
+                if(!isset($formas_pago[$fac->codpago])){
+                    $formas_pago[$fac->codpago] = array();
+                    $facturasprov_formas_pago[$fac->codpago] = array();
+                }
+                if(!isset($formas_pago[$fac->codpago][$fac->coddivisa])){
+                    $formas_pago[$fac->codpago][$fac->coddivisa] = 0;
+                    $facturasprov_formas_pago[$fac->codpago][$fac->coddivisa] = 0;
+                    $facturasprov_pagadas[$fac->coddivisa] = 0;
+                    $facturasprov_porpagar[$fac->coddivisa] = 0;
+                }
+                if($fac->pagada){
+                    $formas_pago[$fac->codpago][$fac->coddivisa] += $fac->total;
+                    $facturasprov_formas_pago[$fac->codpago][$fac->coddivisa] += 1;
+                    $facturasprov_pagadas[$fac->coddivisa] += 1;
+                }else{
+                    $facturasprov_porpagar[$fac->coddivisa] += 1;
+                }
+            }
+            
+            foreach($formas_pago as $codpago=>$list){
+                foreach($list as $divisa=>$v){
+                    if($facturasprov_formas_pago[$codpago][$divisa]){
+                        $item = new stdClass();
+                        $item->codpago = $codpago;
+                        $item->descpago = $fp->get($codpago)->descripcion;
+                        $item->cantidad = $facturasprov_formas_pago[$codpago][$divisa];
+                        $item->divisa = $divisa;
+                        $item->importe = $v;
+                        $resultados_egresos_formas_pago[$divisa][] = $item;
+                    }
+                }
+            }
+        }
+        
+        //Obtenemos las ventas que no estén anuladas y sacamos las que estén o no pagadas
         $query_ventas = "fecha >= ".$this->facturascli->var2str(\date('Y-m-d',strtotime($this->f_desde)))
                 ." AND fecha <= ".$this->facturascli->var2str(\date('Y-m-d',strtotime($this->f_hasta)))
                 ." AND codalmacen = ".$this->facturascli->var2str($this->codalmacen)
@@ -93,6 +172,7 @@ class informes_caja extends fs_controller {
         
         //Obtenemos los cobros de faltantes
         $lista_cobro_faltantes = $this->faltantes->buscar($this->empresa->id, $this->codalmacen, $this->f_desde, $this->f_hasta, FALSE, TRUE);
+        $resultados_faltantes_cobrados = array();
         if($lista_cobro_faltantes){
             $pago_faltante['CONT'] = array();
             $pago_faltante_contador['CONT'] = array();
@@ -104,7 +184,7 @@ class informes_caja extends fs_controller {
                 $pago_faltante['CONT'][$faltante->coddivisa] += $faltante->importe;
                 $pago_faltante_contador['CONT'][$faltante->coddivisa] += 1;
             }
-            $resultados_faltantes_cobrados = array();
+            
             foreach($pago_faltante['CONT'] as $divisa=>$valor){
                 if($pago_faltante_contador['CONT'][$divisa]){
                     $item = new stdClass();
@@ -120,6 +200,7 @@ class informes_caja extends fs_controller {
         
         //Obtenemos los faltantes pendientes
         $lista_faltantes = $this->faltantes->buscar($this->empresa->id, $this->codalmacen, $this->f_desde, $this->f_hasta, FALSE, FALSE);
+        $resultados_faltantes_pendientes = array();
         if($lista_faltantes){
             $recibo_faltante['CONT'] = array();
             $recibo_faltante_contador['CONT'] = array();
@@ -131,7 +212,7 @@ class informes_caja extends fs_controller {
                 $recibo_faltante['CONT'][$faltante->coddivisa] += $faltante->importe;
                 $recibo_faltante_contador['CONT'][$faltante->coddivisa] += 1;
             }
-            $resultados_faltantes_pendientes = array();
+            
             foreach($recibo_faltante['CONT'] as $divisa=>$valor){
                 if($recibo_faltante_contador['CONT'][$divisa]){
                     $item = new stdClass();
@@ -144,6 +225,7 @@ class informes_caja extends fs_controller {
                 }
             }
         }
+        
         if($lista_ingresos){
             $formas_pago = array();
             $facturas_formas_pago = array();
@@ -189,6 +271,7 @@ class informes_caja extends fs_controller {
                 'resumen_movimientos'=>$resultados_movimientos,
                 'faltantes_cobrados'=>$resultados_faltantes_cobrados,
                 'faltantes_pendientes'=>$resultados_faltantes_pendientes,
+                'egresos_formas_pago'=>$resultados_egresos_formas_pago,
                 'egresos'=>$resultados_egresos);
         }else{
             return false;
