6519b03e7611b090e96b42b16e6691b2e0ad9c90
Joe Nilson <joenilson@gmail.com>
2017-02-08 10:28:18 -0400

    unidades_medida: corregido bug cuando el producto no tiene UM en la vista de nueva_compra

--- controller/nueva_compra.php
+++ controller/nueva_compra.php
@@ -371,7 +371,7 @@ class nueva_compra extends fs_controller
          }
 
          //Buscamos las unidades de medida del artículo
-         $this->results[$i]->um_base = ($this->articulo_um->getBase($value->referencia))?$this->articulo_um->getBase($value->referencia)->codum:"DISPLAY";
+         $this->results[$i]->um_base = ($this->articulo_um->getBase($value->referencia))?$this->articulo_um->getBase($value->referencia)->codum:"UNIDAD";
          $this->results[$i]->factor_base = ($this->articulo_um->getBase($value->referencia))?$this->articulo_um->getBase($value->referencia)->factor:1;
          $umAdicionales = $this->articulo_um->getByTipo($value->referencia, 'se_compra');
          $listaUM = "";
